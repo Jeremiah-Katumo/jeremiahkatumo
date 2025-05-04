c07a19dffbe8727039f0978c05f0572354113815
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-05-04 23:42:00 +0300

    merged master into main

---
+++ src/components/Contact/ContactForm.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Row, Col, Form, Container, Button } from 'react-bootstrap';
+
+
+function ContactForm() {
+    return (
+        <Row id='contact-form'>
+            <Col md={4} className='contact-form-container'>
+                <form method="post" className="form-outline-style-v1" id="contactForm">
+                    <div className="form-group contact-form-details row mb-0">
+                        <Col lg={6} className="form-group input-container gsap-reveal">
+                            <Form.Label htmlFor="name">Name</Form.Label>
+                            <Form.Control name="name" type="text" id="name" className="form-control" />
+                        </Col>
+                        <Col lg={6} className="form-group input-container gsap-reveal">
+                            <Form.Label htmlFor="email">Email</Form.Label>
+                            <Form.Control name="email" type="email" id="email" className="form-control" cols="30" />
+                        </Col>
+                        <Col lg={12} className="form-group input-container gsap-reveal">
+                            <Form.Label htmlFor="message">Write your message...</Form.Label>
+                            <Form.Control
+                                as="textarea"
+                                name="message"
+                                id="message"
+                                cols="50"
+                                rows="7"
+                                className="form-control"
+                            />
+                        </Col>
+                    </div>
+                    <div className="form-group contact-form-details row gsap-reveal">
+                        <Button href="#" className="btn btn-primary resume-btn">
+                            Send Message
+                        </Button>
+                    </div>
+                </form>
+
+                {/* <div id="form-message-warning" className="mt-4"></div>
+                <div id="form-message-success">Your message was sent, thank you!</div> */}
+            </Col>
+
+            <Col md={4} className='contact-info'>
+                <div className="contact-info-v1">
+                    <div className="gsap-reveal d-block contact-card">
+                        <span className="d-block contact-info-label"><p>Email</p></span>
+                        <a href="#" className="contact-info-val">jeremykurwa02@gmail.com</a>
+                    </div>
+                    <div className="gsap-reveal d-block contact-card">
+                        <span className="d-block contact-info-label"><p>Phone</p></span>
+                        <a href="#" className="contact-info-val">+25473 6789 012</a>
+                    </div>
+                    <div className="gsap-reveal d-block contact-card">
+                        <span className="d-block contact-info-label"><p>Address</p></span>
+                        <address className="contact-info-val">
+                            Kilifi - Mombasa Road. <br /> Kilifi Town - Vidazini Road
+                        </address>
+                    </div>
+                </div>
+            </Col>
+        </Row>
+    );
+}
+
+export default ContactForm;
