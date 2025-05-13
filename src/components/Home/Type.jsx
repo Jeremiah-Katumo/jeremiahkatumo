017d5f340cd44520584da0c142282cd1de07bc4d
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-05-13 15:00:10 +0300

    styled typewriter

--- src/components/Home/Type.jsx
+++ src/components/Home/Type.jsx
@@ -4,26 +4,28 @@ import Typewriter from "typewriter-effect";
 function Type() {
   return (
     <div>
-    <Typewriter options={{
-      strings: [
-        "Data Scientist",
-        "Statistician",
-        "Software Engineer",
-        "Freelancer",
-      ],
-      autoStart: true,
-      loop: true,
-      deleteSpeed: 50,
-      cursor: "|",
-      callback: function() {
-        console.log("Typed!");
-      },
-      fontSize: "10em",
-      display: "flex",
-      justifyContent: "left",
-      alignItems: "start",
-      color: "blue"
-    }} />
+      <span className="typewriter-text">
+        <Typewriter options={{
+          strings: [
+            "Data Scientist",
+            "Statistician",
+            "Software Engineer",
+            "Freelancer",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+          cursor: "|",
+          callback: function () {
+            console.log("Typed!");
+          },
+          fontSize: "10em",
+          display: "flex",
+          justifyContent: "left",
+          alignItems: "start",
+          color: "blue"
+        }} />
+      </span>
     </div>
   );
 }
