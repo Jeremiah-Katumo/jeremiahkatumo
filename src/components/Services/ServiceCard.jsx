f728f587384721d2d55599bac7d95be9cc4db344
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-05-15 20:51:43 +0300

    service card indentation

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -11,15 +11,15 @@ const ServiceCard = ({ data }) => {
         <>
             {data.map((item, idx) => (
                 <div key={idx} class="col-md-4 text-center d-flex service-card">
-                <a href="#" class="services-1">
-                    <span class="icon">
-                        <i class={item.icon}></i>
-                    </span>
-                    <div class="desc">
-                        <h3 class="mb-5">{item.heading}</h3>
-                    </div>
-                </a>
-            </div>
+                    <a href="#" class="services-1">
+                        <span class="icon">
+                            <i class={item.icon}></i>
+                        </span>
+                        <div class="desc">
+                            <h3 class="mb-5">{item.heading}</h3>
+                        </div>
+                    </a>
+                </div>
             ))}
         </>
     )
