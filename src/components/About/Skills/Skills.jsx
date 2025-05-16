2b6b3c055fee8c7242889c42077645a893bd3fe3
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-05-16 16:17:05 +0300

    WIP

--- src/components/About/Skills/Skills.jsx
+++ src/components/About/Skills/Skills.jsx
@@ -9,7 +9,7 @@ function Skills() {
     <section className="skills-content-section" id="skills">
       <div className="section-header">
         <h2 className="section-title">My Skills</h2>
-        <p>Here are some of my proficiencies</p>
+        <p>A mix of what I do and what I love. From programming languages and <br />frameworks to design tools and personal passions - this section <br />reflects my professional strengths and what keeps me curious.</p>
       </div>
 
       <div className="skills-container">
