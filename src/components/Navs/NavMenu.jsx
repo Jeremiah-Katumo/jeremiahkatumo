ad0a972c53163faba6ce30681f32acaa25cd7533
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-05-18 02:57:32 +0300

    added a mobile nav toggle dropdown men

--- src/components/Navs/NavMenu.jsx
+++ src/components/Navs/NavMenu.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import { Nav, Navbar, Container } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
 
 const NavMenu = () => {
-
     return (
-        <>
-            <Container className="nav-list-content">
-                <Navbar.Collapse id="ftco-nav">
-                    <Nav className="ml-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/about">About</Nav.Link>
-                        <Nav.Link href="/resume">Resume</Nav.Link>
-                        <Nav.Link href="/services">Services</Nav.Link>
-                        {/* <Nav.Link href="#skills-section">Skills</Nav.Link> */}
-                        <Nav.Link href="/projects">Projects</Nav.Link>
-                        <Nav.Link href="/blogs">Blog</Nav.Link>
-                        <Nav.Link href="/contacts">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </>
-    )
-}
+        <div className="navmenu-container">
+            <Nav className="flex-column flex-lg-row text-end text-lg-start w-30">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/resume">Resume</Nav.Link>
+                <Nav.Link href="/services">Services</Nav.Link>
+                <Nav.Link href="/projects">Projects</Nav.Link>
+                <Nav.Link href="/blogs">Blog</Nav.Link>
+                <Nav.Link href="/contacts">Contact</Nav.Link>
+            </Nav>
+        </div>
+    );
+};
 
-export default NavMenu
+export default NavMenu;
