a26ee25c0fda799683fbf1e2ded50ccc0aeea20d
Jeremiah-Katumo <jeremykatush@gmail.com>
2025-05-27 00:59:42 +0300

    WIP

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,27 +21,12 @@ function Home() {
               </Col>
             </Row>
 
-            <Container>
+            <Container className='bio-text'>
               <Row>
                 <h5>Passionate about building tech for social impact.</h5>
               </Row>
             </Container>
-
-            {/* <Container>
-              <Row className="social-links">
-                <a href="https://x.com/jeremy_warku" className="x-twitter"><i className="bi bi-twitter"></i></a>
-                <a href="https://github.com/Jeremiah-Katumo" className="github"><i className="bi bi-github"></i></a>
-                <a href="https://www.instagram.com/jeremykatumo/profilecard/?igsh=N2pwaDlpcmU4b3k4" className="instagram"><i className="bi bi-instagram"></i></a>
-                <a href="https://www.linkedin.com/in/jeremiah-kurwa-27556b230/" className="linkedin"><i className="bi bi-linkedin"></i></a>
-                <a href="medium.com/@jeremykatush" className="medium"><i className="bi bi-medium"></i></a>
-              </Row>
-            </Container> */}
           </Container>
-          {/* <Container className='social-links'> */}
-          {/* <Button href="#my-about">
-            <a className="find-out-more-btn" href="#about">Find Out More</a>
-          </Button> */}
-          {/* </Container> */}
           <CustomButton href={"#my-about"} className="find-out-more-btn" label="Find Out More" />
         </Container>
       </Container>
